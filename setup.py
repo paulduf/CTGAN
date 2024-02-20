3ec1a8430c91491b1394c0ccbb4a31cbd90a65f8
DUFOSSE Paul <paulduf@users.noreply.github.com>
2024-02-20 18:07:51 +0100

    fix: version number

--- setup.py
+++ setup.py
@@ -119,6 +119,6 @@ setup(
     test_suite="tests",
     tests_require=tests_require,
     url="https://github.com/sdv-dev/CTGAN",
-    version="0.888",
+    version="0.7.7dev",
     zip_safe=False,
 )
